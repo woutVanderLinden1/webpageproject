4b2e55292684bcf6f5a0aa4d3e7d4d7c6def32fc
BarbAme <barbara.ameloot@student.kuleuven.be>
2019-11-19 17:15:52 +0100

    Added popups

--- src/pages/recommendations.js
+++ src/pages/recommendations.js
@@ -22,6 +22,122 @@ class Recommendations extends React.Component {
 
     }
 
+    //POPUP CODE
+    nutritionalPopup(name, food){
+       //TODO get nutritional info
+        let nutritionIcons = [<button className="IconLayout NutritionIcon"></button> ];
+
+        let nutritionLabels = ['Carbs','Total Fats', 'Sugar', 'Sodium', 'Protein', 'Saturated Fats', 'Carbohydrates'];
+        let nutritionalInfo = this.getNutritionInfo(name);
+        let nutritionalInfoHtml = []
+        nutritionalInfoHtml.push(<div className="Recipe">
+                Nutritional info:
+            </div>
+        );
+        for (let i = 0; i < nutritionalInfo.length; i++){
+
+            nutritionalInfoHtml.push(
+                <div className = "Recipe">
+                    {nutritionLabels[i]}: {nutritionalInfo[i]}
+                    <br/>
+
+                </div>)
+        }
+        let html = [
+            <Popup modal trigger={
+                <div>
+                    <button>
+                        {nutritionIcons}
+                    </button>
+                </div>} position="right center">
+                <div className="RecipeTitle"><h2> Nutritional info for {name}</h2></div>
+                <br/>
+
+                {nutritionalInfoHtml}
+
+            </Popup>]
+
+        return html;
+    }
+    recipePopup(name, food){
+        let recipeIcons = [<button className="IconLayout RecipeIcon"></button> ];
+        //Recipe list
+        let recept = this.getRecipe(name);
+        let recipeHtml = []
+        recipeHtml.push(<div className="Recipe">
+                Steps:
+            </div>
+        );
+        for (let i = 0; i < recept.length; i++){
+
+            recipeHtml.push(
+                <div className = "Recipe">
+                    {i + 1}. {recept[i]}
+                    <br/>
+
+                </div>)
+        }
+
+        //Ingredients list
+        let ingredients = this.getIngredients(name);
+        let ingredientsHtml = []
+        ingredientsHtml.push(<div className="Recipe">
+                Ingredients:
+            </div>
+        );
+        for (let i = 0; i < ingredients.length; i++){
+            ingredientsHtml.push(
+                <div className = "Recipe">
+                    {ingredients[i]}
+                    <br/>
+
+                </div>)
+        }
+
+        let html = [
+            <Popup modal trigger={
+                <div>
+                    <button>
+                        {recipeIcons}
+                    </button>
+                </div>} position="right center">
+                <div className="RecipeTitle"><h2>Recipe for {name}</h2></div>
+                <br/>
+
+                {ingredientsHtml}
+
+                {recipeHtml}
+            </Popup>]
+
+        return html;
+
+    }
+
+    //GET DATA
+    getIngredients(gerecht){
+        //TODO implementeren
+        return [['pineapple tidbits'],[ 'vegetable oil'],[ 'garlic cloves'],[ 'yellow onion'],[ 'celery'],[ 'rice'],[ 'salt'],[ 'ground cumin'],[ 'ground ginger'],[ 'turmeric'],[ 'cayenne pepper'],[ 'chicken broth'],[ 'raisins'],[ 'frozen peas'],[ 'green onion']];
+     }
+    getRecipe(gerecht){
+        //TODO implementeren
+        return [['drain pineapple' ],[ 'reserve juice'],[ 'heat oil in large skillet'],[ 'saute garlic '],[' onion '],[' and celery until onion is soft'],[ 'stir in rice' ],[ 'reserved juice' ],[ 'seasonings '],[ 'chicken broth and raisins'],[ 'bring to a boil'],[ 'cover '],[ 'simmer 20 minutes until rice is tender'],[ 'stir in peas '],[' green onion and pineapple'] ];
+
+    }
+    getNutritionInfo(gerecht){
+        //TODO implementeren
+        //volgorde:
+        // Calories in g
+        // Total fat in %
+        // Suiker in %
+        // Sodium in %
+        // Protein in %
+        // Saturated Fat in %
+        // Total Carbohydrate in %
+        return [428.5, 12.0, 112.0, 41.0, 19.0, 6.0, 27.0];
+
+    }
+
+    //GENERATE MEAL
     generateMeal() {
         let foods = ["Spicy rice", "Curry chicken", "Thai noodles", "Veggie Burritos", "Fish pie"];
         let images = ["https://www.dinneratthezoo.com/wp-content/uploads/2017/10/firecracker-chicken-1.jpg",
@@ -36,26 +152,35 @@ class Recommendations extends React.Component {
             ["Veggie", "Cheap"],
             ["Fish", "Oven"]
         ]
+
+
         let html = [];
 
+
         for (let i = 1; i <= 5; i++) {
+
+
             let badges = [];
             assets[i-1].forEach(function (item, index) {
                 let badgeName = "FoodBadge " + item;
                 console.log(badgeName);
                 badges.push(<button className={badgeName}> </button>);
+
             });
             let className = "FoodItem fadeInLeft" + i;
-            html.push(<Popup modal trigger={<div>
+            html.push(<div>
                                                 <button className={className} onClick={this.increaseCounter}>
                                                     <img className="FoodPhoto" align="left" src={images[i-1]} alt="Food"/>
                                                     <b className="FoodTitle">{foods[i-1]}</b> <br/>
                                                     {badges}
+
+                                                    {this.recipePopup(foods[i-1], i-1)}
+                                                    {this.nutritionalPopup(foods[i-1], i-1)}
                                                 </button>
-                                            </div>} position="right center">
-                        <div><h2>Recipe for {foods[i-1]}</h2></div>
-                    </Popup>)
+                                                </div>)
+
         }
+
         return html
     }
     render() {
