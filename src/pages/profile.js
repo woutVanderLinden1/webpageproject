e0b30419ccc6525e473cfc423c7ba3b9c781fae6
Maxwell <maxwell.szymanski@student.kuleuven.be>
2019-12-16 15:54:28 +0100

    Update

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -110,7 +110,7 @@ class Profile extends React.Component {
         return (
 
             <div className="App">
-                <div className="PageHeader"> <b className="PageTitle">Profile</b>
+                <div className="PageHeader"> <b className="PageTitle">Let's set you up.</b>
                 </div>
                 <header className="App-header">
                     {this.getRender()}
